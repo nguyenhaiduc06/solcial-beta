2a28412092597f54114330e5f641ebd71ab8bd0a
Nguyen Hai Duc <nguyenhaiduc06@gmail.com>
2023-10-08 23:14:17 +0700

    UPDATE: profile test

--- tests/profile.ts
+++ tests/profile.ts
@@ -1,32 +1,71 @@
 import * as anchor from "@coral-xyz/anchor";
 import { user, program } from ".";
 import { PublicKey } from "@solana/web3.js";
-import { expect } from "chai";
+import { assert } from "chai";
+
+async function createProfile(name, username, bio) {
+  const [profilePDA, _] = PublicKey.findProgramAddressSync(
+    [anchor.utils.bytes.utf8.encode("profile"), user.publicKey.toBuffer()],
+    program.programId
+  );
+
+  await program.methods
+    .createProfile(name, username, bio)
+    .accounts({
+      user: user.publicKey,
+      profile: profilePDA,
+    })
+    .rpc();
+
+  const profile = await program.account.profile.fetch(profilePDA);
+
+  return profile;
+}
 
 describe("profile", () => {
+  it("cannot create a new profile with name longer than 20 characters", async () => {
+    try {
+      await createProfile("x".repeat(21), "username", "bio");
+      assert.fail(
+        "still can create a new profile with name longer than 20 characters"
+      );
+    } catch (error) {
+      assert.equal(error.error.errorCode.code, "TooLong");
+    }
+  });
+
+  it("cannot create a new profile with username longer than 20 characters", async () => {
+    try {
+      await createProfile("name", "x".repeat(21), "bio");
+      assert.fail(
+        "still can create a new profile with username longer than 20 characters"
+      );
+    } catch (error) {
+      assert.equal(error.error.errorCode.code, "TooLong");
+    }
+  });
+
+  it("cannot create a new profile with bio longer than 50 characters", async () => {
+    try {
+      await createProfile("name", "username", "x".repeat(51));
+      assert.fail(
+        "still can create a new profile with bio longer than 50 characters"
+      );
+    } catch (error) {
+      assert.equal(error.error.errorCode.code, "TooLong");
+    }
+  });
+
   it("can create a new profile!", async () => {
     const name = "Name",
       username = "username",
       bio = "Biography 🌲";
 
-    const [profilePDA, _] = PublicKey.findProgramAddressSync(
-      [anchor.utils.bytes.utf8.encode("profile"), user.publicKey.toBuffer()],
-      program.programId
-    );
+    const profile = await createProfile(name, username, bio);
 
-    await program.methods
-      .createProfile(name, username, bio)
-      .accounts({
-        user: user.publicKey,
-        profile: profilePDA,
-      })
-      .rpc();
-
-    const profile = await program.account.profile.fetch(profilePDA);
-
-    expect(profile.name).to.equal(name);
-    expect(profile.username).to.equal(username);
-    expect(profile.bio).to.equal(bio);
+    assert.equal(profile.name, name);
+    assert.equal(profile.username, username);
+    assert.equal(profile.bio, bio);
   });
 
   it("can update profile!", async () => {
@@ -49,9 +88,9 @@ describe("profile", () => {
 
     const profile = await program.account.profile.fetch(profilePDA);
 
-    expect(profile.name).to.equal(newName);
-    expect(profile.username).to.equal(newUsername);
-    expect(profile.bio).to.equal(newBio);
+    assert.equal(profile.name, newName);
+    assert.equal(profile.username, newUsername);
+    assert.equal(profile.bio, newBio);
   });
 
   it("can delete profile", async () => {
@@ -70,6 +109,6 @@ describe("profile", () => {
 
     const profile = await program.account.profile.fetchNullable(profilePDA);
 
-    expect(profile).to.be.null;
+    assert.equal(profile, null);
   });
 });
